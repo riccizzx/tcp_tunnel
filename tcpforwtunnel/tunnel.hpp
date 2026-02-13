8bdf26f0f6d32d4808ee014bc6d8583cf5ead2a8
angel <guilhermericci1445@gmail.com>
2026-02-13 12:10:35 -0300

    description

--- tcpforwtunnel/tunnel.hpp
+++ tcpforwtunnel/tunnel.hpp
@@ -1,13 +1,24 @@
 
 #include "headers.hpp"
 
+/*
+	this file contains the main logic of the tunnel and the configuration of the tunnel.
+	you can modify the configuration in the config namespace and the main logic in the functions below additionally
+	you can test by your own, change the remote server and port to test with your own server, or you can use the provided echo server in the repo for testing
+	for ethical and legal reasons, don't use this tunnel for malicious purposes, and don't use it to attack or hack any server without permission, this tunnel is for educational purposes only
+	you can add more functions for more advanced features like encryption, compression, etc...
+ 
+ */
+
 namespace config
 {
 
 	constexpr int BUFFER_SIZE = 4096;
 	constexpr int LOCAL_PORT = 9909;
 	constexpr const char* REMOTE_IP = "127.0.0.1";
-	constexpr int REMOTE_PORT = 49667;
+	//constexpr const char* REMOTE_IP = "13.107.18.254";
+	constexpr int REMOTE_PORT = 8080; // 61079
+	//constexpr int REMOTE_PORT = 443; // 61079
 
 };
 
